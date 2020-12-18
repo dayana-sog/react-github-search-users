85e5232ec25d859d3b630b940a07341563cd7ff5
dayana-sog <dayanasog23@gmail.com>
2020-12-18 12:48:58 +0000

    criação dados do usuário

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,44 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { GithubContext } from '../context/context';
 import styled from 'styled-components';
 import { MdBusiness, MdLocationOn, MdLink } from 'react-icons/md';
 const Card = () => {
-  return <h2>card component</h2>;
+  const { githubUser } = useContext(GithubContext);
+  const { 
+    avatar_url, 
+    html_url, 
+    name, 
+    company, 
+    blog, 
+    bio, 
+    location, 
+    twitter_username 
+  } = githubUser;
+
+  return (
+    <Wrapper>
+      <header>
+        <img src={avatar_url} alt={name}/>
+        <div>
+          <h4>{name}</h4>
+          <p>@{twitter_username || name.toLowerCase()}</p>
+        </div>
+        <a href={html_url}>follow</a>
+      </header>
+      <p className="bio">{bio}</p>
+      <div className="links">
+        <p>
+          <MdBusiness/> {company || 'uninformed'}
+        </p>
+        <p>
+          <MdLocationOn/> {location || 'uninformed'}
+        </p>
+      </div>
+      <a href={`https://${blog}`}>
+        <MdLink/> {blog || 'uninformed'}
+      </a>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.article`
   background: var(--clr-white);
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -5,11 +5,11 @@ import { GithubContext } from '../context/context';
 const Dashboard = () => {
   return (
     <main>
-      <Navbar />
-      <Search /> 
+      {/* <Navbar /> */}
+      {/* <Search />  */}
       <Info />
       <User />
-      <Repos />
+      {/* <Repos /> */}
     </main>
   );
 };
